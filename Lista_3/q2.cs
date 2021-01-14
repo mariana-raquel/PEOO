715aa5a9f120a91bf2716ca51ef6bf03d09a7cf6
Mariana Raquel <48134840+mariana-raquel@users.noreply.github.com>
2021-01-14 19:44:22 -0300

    Create q2.cs

---
+++ Lista_3/q2.cs
@@ -0,0 +1,14 @@
+using System;
+  class MainClass {
+    public static void Main (string[] args) {
+      string[] e = Console.ReadLine().Split(' ');
+      int a = int.Parse(e[0]);
+      int b = int.Parse(e[1]);
+      int c = int.Parse(e[2]);
+
+      int m1 = Math.Max(a, b);
+      int m2 = Math.Max(m1, c);
+
+      Console.WriteLine($"{m2} eh o maior");  
+  }
+}
