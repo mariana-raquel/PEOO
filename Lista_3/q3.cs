e4b503295cc3550bfd1f3892d92d3e463dc915ad
Mariana Raquel <48134840+mariana-raquel@users.noreply.github.com>
2021-01-14 19:44:50 -0300

    Create q3.cs

---
+++ Lista_3/q3.cs
@@ -0,0 +1,11 @@
+using System;
+  class MainClass {
+    public static void Main (string[] args) {
+      double a = double.Parse(Console.ReadLine());
+      double b = double.Parse(Console.ReadLine());
+    
+      double dp = a / b;
+
+      Console.WriteLine($"{dp:0.000} km/l");
+  }
+}
