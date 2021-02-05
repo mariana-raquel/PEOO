8437a4b3500a660bbb8c06e3979fa8cc1720e501
Mariana Raquel <marianaquel.morais@gmail.com>
2021-02-05 13:55:02 +0000

    lista 8

--- Lista_1/q1.cs
+++ Lista_1/q1.cs
@@ -1,6 +1,13 @@
 using System;
   class MainClass {
-    public static void Main (string[] args) {
-      Console.WriteLine("Mariana");  
+    public static void Main(string[] args) {
+      double c
+    }
+    class Circulo {
+      public double r;
+      public double p;
+      p = 3.14159;
+      double area = p * r * r;
+      return area;
+    }
   }
-}
