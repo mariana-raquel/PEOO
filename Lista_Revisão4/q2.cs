9bb4b6baf7586a2a4968cdde5866baa4ab26773f
Mariana Raquel <>
2021-04-27 15:32:29 +0000

    .

--- Lista_Revisão4/q2.cs
+++ Lista_Revisão4/q2.cs
@@ -2,6 +2,65 @@ using System;
 using System.Collections.Generic;
   class MainClass {
     public static void Main(string[] args) {
+      Construtora c = new Construtora();
+      Funcionario f1 = new Funcionario();
+      f1.SetNome("Mariana");
+      f1.SetEmail("aeiou");
+      f1.SetFone("1234-5678");
+
+      Motorista f2 = new Motorista();
+      f2.SetNome("Adriana");
+      f2.SetEmail("bcdfg");
+      f2.SetFone("8765-4321");
+      f2.SetCnh("cccccc");
+
+      Engenheiro f3 = new Engenheiro();
+      f3.SetNome("José");
+      f3.SetEmail("hjklm");
+      f3.SetFone("1357-91113");
+      f3.SetCrea("aaaaaa");
+
+      Motorista f4 = new Motorista();
+      f4.SetNome("João");
+      f4.SetEmail("npqrs");
+      f4.SetFone("2468-1012");
+      f4.SetCnh("dddddd");
+
+      Engenheiro f5 = new Engenheiro();
+      f5.SetNome("Maria");
+      f5.SetEmail("tvwxy");
+      f5.SetFone("1517-1921");
+      f5.SetCrea("bbbbbb");
+
+      Funcionario f6 = new Funcionario();
+      f6.SetNome("Juan");
+      f6.SetEmail("zabcd");
+      f6.SetFone("1416-1820");
+
+      c.Inserir(f1);
+      c.Inserir(f2);
+      c.Inserir(f3);
+      c.Inserir(f4);
+      c.Inserir(f5);
+      c.Inserir(f6);
+
+      Console.WriteLine("Todos os funcionários:");
+      foreach (Construtora x in c.Funcionarios()) {
+        Console.WriteLine(x);
+      }
+      Console.WriteLine();
+
+      Console.WriteLine("Engenheiros:");
+      foreach (Construtora y in c.Engenheiros()) {
+        Console.WriteLine(y);
+      }
+      Console.WriteLine();
+
+      Console.WriteLine("Motoristas:");
+      foreach (Construtora z in c.Motoristas()) {
+        Console.WriteLine(z);
+      }
+
 
     }
   }
@@ -10,15 +69,27 @@ using System.Collections.Generic;
     public void Inserir(Funcionario f) {
       funcs.Add(f);
     }
-    /*public List<Funcionario> Funcionarios() {
-
+    public List<Funcionario> Funcionarios() {
+      return funcs;
     }
     public List<Engenheiro> Engenheiros() {
-
+      List<Engenheiro> engs = new List<Engenheiro>();
+      foreach (Funcionario x in Funcionarios()) {
+        if (x is Engenheiro) {
+          engs.Add(x as Engenheiro);
+        }
+      }
+      return engs;
     }
     public List<Motorista> Motoristas() {
-
-    }*/
+      List<Motorista> moto = new List<Motorista>();
+      foreach (Funcionario x in Funcionarios()) {
+        if (x is Motorista) {
+          moto.Add(x as Motorista);
+        }
+      }
+      return moto;
+    }
   }
 
   class Funcionario : Construtora {
@@ -55,7 +126,7 @@ using System.Collections.Generic;
       return crea;
     }
     public override string ToString() {
-      return $"Crea: {crea}";
+      return $"Nome: {base.GetNome()} - Email: {base.GetEmail()} - Fone: {base.GetFone()} - Crea: {crea}";
     }
   }
 
@@ -68,6 +139,6 @@ using System.Collections.Generic;
       return cnh;
     }
     public override string ToString() {
-      return $"Cnh: {cnh}";
+      return $"Nome: {base.GetNome()} - Email: {base.GetEmail()} - Fone: {base.GetFone()} - Cnh: {cnh}";
     }
   }
--- Lista_Revisão4/q3.cs
+++ Lista_Revisão4/q3.cs
@@ -1,6 +1,84 @@
 using System;
   class MainClass {
     public static void Main(string[] args) {
-      
+      Paciente p1 = new Paciente("Maria", "123.456.789-10", "91112-1314", DateTime.Parse("01-01-2002"));
+      Paciente p2 = new Paciente("José", "151.617.181-9", "92021-2223", DateTime.Parse("03-10-2003"));
+      Paciente p3 = new Paciente("Ana", "242.526.272-8", "92930-3132", DateTime.Parse("06-20-2004"));
+      Paciente p4 = new Paciente("João", "333.435.363-7", "93839-4041", DateTime.Parse("04-27-2021"));
+
+      Hospital h = new Hospital();
+      h.Inserir(p1);
+      h.Inserir(p2);
+      h.Inserir(p3);
+      h.Inserir(p4);
+
+      foreach (Paciente x in h.Listar()) {
+        Console.WriteLine(x);
+      }
     }
+  }
+  class Hospital {
+    private Paciente[] paciente = new Paciente[20];
+    private int k;
+    public void Inserir(Paciente p) {
+      if (k < 20) {
+        paciente[k++] = p;
+      }
+    }
+    public Paciente[] Listar() {
+      Paciente[] novoPaciente = new Paciente[k];
+      Array.Copy(paciente, novoPaciente, k);
+      return novoPaciente;
+    }
+  }
+  class Paciente {
+    private string nome, cpf, telefone;
+    private DateTime nascimento;
+    public string Idade {
+      get {
+        int anos = 0;
+        int meses = 0;
+        DateTime hoje = DateTime.Now;
+        if (hoje.Month > nascimento.Month) {
+          anos += (hoje.Year - nascimento.Year);
+          meses += (hoje.Month - nascimento.Month);
+        }
+        if (hoje.Month < nascimento.Month) {
+          anos += (hoje.Year - nascimento.Year) - 1;
+          meses += (hoje.Month - nascimento.Month) + 12;
+        }
+        if (hoje.Month == nascimento.Month && hoje.Day > nascimento.Day) {
+          anos += (hoje.Year - nascimento.Year);
+          meses += (hoje.Month - nascimento.Month);
+        }
+        if (hoje.Month == nascimento.Month && hoje.Day < nascimento.Day) {
+          anos += (hoje.Year - nascimento.Year) - 1;
+          meses += (hoje.Month - nascimento.Month) + 12;
+        }
+        if (hoje.Month == nascimento.Month && hoje.Day == nascimento.Day) {
+          anos += (hoje.Year - nascimento.Year);
+          meses += (hoje.Month - nascimento.Month);
+        }
+        return $"{anos} anos e {meses} meses";
+      }
+    } 
+    public Paciente(string n, string c, string t, DateTime d) {
+      try {
+      if (n.Length > 0) nome = n;
+      if (c.Length > 0) cpf = c;
+      if (t.Length > 0) telefone = t;
+      if (d.Day > 0 && d.Month > 0) nascimento = d;
+      if (d.Year == DateTime.Now.Year && d.Month == DateTime.Now.Month && d.Day == DateTime.Now.Day|| d > DateTime.Now) throw new DataInvalidaException($"O Paciente {nome} foi cadastrado com a data de nascimento inválida.");
+      }
+      catch (DataInvalidaException obj) {
+        Console.WriteLine(obj.Message);
+        Console.WriteLine();
+      }
+    }
+    public override string ToString() {
+      return $"Nome: {nome} - CPF: {cpf} - Telefone: {telefone} - Data de nascimento: {nascimento:dd/MM/yyyy} - Idade: {Idade}";
+    }
+  }
+  class DataInvalidaException : Exception {
+    public DataInvalidaException(string m) : base(m) {}
   }
